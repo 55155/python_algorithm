0e68933e9009cbcd8aaea35eb4b2f4d711cba81d
55155 <qkdtjdwls1024@gmail.com>
2024-05-07 23:59:49 +0900

    20240507

--- 문제 풀이/DP/1로만들기2_12852.py
+++ 문제 풀이/DP/1로만들기2_12852.py
@@ -9,6 +9,7 @@ for i in range(2, N+1):
     DP[i][1] = DP[i-1][1] + [i]
 print(DP[-1][0])
 DP[-1][1].reverse()
+for i in range(1, N+1)
     if i % 3 == 0 and DP[i][0] > (DP[i // 3][0] + 1):
         DP[i][0] = DP[i // 3][0] + 1
         DP[i][1] = DP[i // 3][1] + [i]
--- 문제 풀이/DP/가장큰정사각형_1915.py
+++ 문제 풀이/DP/가장큰정사각형_1915.py
@@ -1,9 +1,23 @@
 # 입력부
+import copy
 N, M = map(int, input().split())
-board = []
-for i in range(N):
-    temp = list(map(int, input().split()))
-    board.append(temp)
+board = [[] for i in range(N)]
 
+for row in range(N):
+    temp = input()
+    for col in range(M):
+        board[row].append(int(temp[col]))
+    
+direction = []
+DP = copy.deepcopy(board)
 # 구현부
-def square()
+for row in range(1,N):
+    for col in range(1,M):
+        if DP[row][col] != 0:
+            DP[row][col] = min(DP[row-1][col], DP[row][col-1], DP[row-1][col-1]) + 1
+max_value = 0
+for i in DP:
+    max_line = max(i)
+    if max_line > max_value:
+        max_value = max_line
+print(max_value ** 2)
